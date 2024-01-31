950cabbfed12e0487f8ad40ba22056492fb1f1ed
keshavjha123 <31789243+keshavjha123@users.noreply.github.com>
2024-01-31 17:11:12 +0530

    commented out some part

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 import FooterComponent from './components/FooterComponent'
 import BottomInfo from './components/BottomInfo'
 import ContentComponent from './components/ContentComponent'
-import { Route, Routes } from 'react-router'
+// import { Route, Routes } from 'react-router'
 import MainPage from './pages/MainPage'
 import { Routes, Route } from 'react-router-dom';
 
